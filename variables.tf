a1b6c1650ebe0b47066517d86d3e982725210303
ibrahim <iirasras@outlook.com>
2023-07-29 12:14:57 +1000

    Start Terraform Azure

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+}
+
+variable "app_service_name" {
+  description = "The name of the Azure Web App Service."
+}
